baa90b87b6028ccfbc59468b244beb556b30c63a
Shourya Naik <sjnaik02@gmail.com>
2022-07-13 12:02:16 +0530

    added a basic implementation of apod application

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+  let data = '';
+  async function getApod(){
+    const res = await fetch("https://apod.ellanan.com/api");
+    data = await res.json();
+    return data;
+  }
+  let promise = getApod();
+  onMount(() => getApod());
+</script>
+
+{#await promise}
+  <h2> Loading....</h2> 
+{:then data} 
+  <div class="wrapper">
+    <h1>APOD</h1>
+    <img srcset="{data.url} 1x,
+                 {data.hdurl} 2x" alt="The APOD."/>
+    <p>{data.explanation}</p>
+  </div>
+{/await}
+
+
+
+<style>
+  .wrapper{
+    height: 100vh;
+    width: 100vw;
+  }
+  h1{
+    margin: 0px;
+    text-align: center;
+    font-family: sans-serif;
+    color: black;
+    grid-area: a;
+  }
+  img{
+    aspect-ratio: auto;
+    width: 40vw;
+
+  }
+  p{
+    color: black;
+    font-family: sans-serif;
+    text-align: center;
+
+    margin: 1rem;
+  }
+  
+</style>
