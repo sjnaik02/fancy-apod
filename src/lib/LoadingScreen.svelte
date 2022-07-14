281c3788fefe87dbd068dcd0e19fca79c31ff1e7
Shourya Naik <sjnaik02@gmail.com>
2022-07-14 20:48:22 +0530

    Added Simple Loading Element

---
+++ src/lib/LoadingScreen.svelte
@@ -0,0 +1,17 @@
+<div class="load-wrapper">
+  <h1> Loading... </h1>
+</div>
+
+<style>
+  h1{
+    font-family: 'Anton', serif;
+  }
+  .load-wrapper{
+    background-color: darkslategrey;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
