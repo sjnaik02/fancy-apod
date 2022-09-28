4fac6427b05799f52a1f41815b7969e1e1881fad
Shourya Naik <sjnaik02@gmail.com>
2022-09-28 13:53:56 +0530

    added the beginnings of ssr

---
+++ src/routes/[date].svelte
@@ -0,0 +1,179 @@
+<script context="module">
+  export async function load({ fetch, params }){
+    const date = params.date
+    const res = await fetch(`https://apod.ellanan.com/api?date=${date}`);
+    const apod = await res.json();
+
+    if(res.ok){
+      return{
+        props: {
+          apod
+        }
+      }
+    }
+
+    return {
+      status: res.status,
+      error: new Error('could not fetch apod')
+    }
+  }
+</script>
+
+<script>
+  export let apod;
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Anton&family=Space+Grotesk&display=swap" rel="stylesheet">
+</svelte:head>
+<div class="wrapper" style="background-image: url({apod.hdurl}), url({apod.url});">
+  <div class="title-bar">
+    <h1>Astronomy Picture of the Day</h1>
+  </div>
+  <div class="content">
+    <article>
+      <h2>{apod.title}</h2>
+      <p>{apod.explanation}</p>
+    </article>
+    <div class="nav-buttons">
+      <a id="decrement" href="/"> &lt; </a>
+      <a id="increment" href="/" style="pointer-events: {apod.date == new Date().toISOString().slice(0,10) ? "none" : "auto" }"> &gt; </a>
+    </div>
+  </div>
+</div>
+
+<style>
+  :global(body){
+    background-color: #0e0e0e;
+  }
+  .wrapper{
+    height: 100vh;
+    width: 100vw;
+    background-color: #000;
+    background-position: center;
+    background-size: cover;
+    display: grid;
+    grid-template-columns: 5rem 1fr;
+  }
+  .title-bar{
+    padding: 0 1rem 0 1rem;
+    width: 3rem;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-right: 1px solid white;
+    background-color: rgba(0, 0, 0, 0.5);
+    grid-column: 1;
+  }
+  .content{
+    grid-column: 2;
+    height: 100vh;
+    width: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: grid;
+    grid-template-columns: 3fr 3fr 0.5fr;
+    align-items: center;
+    justify-content: center;
+    grid-template-rows: 100vh;
+  }
+  .nav-buttons{
+    grid-column: 3;
+    justify-self: right;
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+  }
+  h1{
+    font-family: 'Anton', sans-serif;
+    color: #fff;
+    writing-mode: vertical-rl; 
+    transform: rotate(180deg);
+    letter-spacing: 0.3rem;
+    font-size: 2.5rem;
+  }
+  h2{
+    font-family: 'Space Grotesk', sans-serif;
+    color: #fff;
+    font-size: 2rem;
+  }
+  p{
+    color: #fff;
+    font-family: 'Space Grotesk', sans-serif;
+    font-size: 1rem;
+  }
+  article{
+    grid-column: 2;
+    justify-self: center;
+    margin-left: 2rem;
+  }
+  #increment, #decrement{
+    background-color: rgba(0, 0, 0, 0);
+    font-size: 2.5rem;
+    color: yellow;
+    outline: none;
+    border: 0;
+    text-align: end;
+    padding-right: 3rem;
+    width: fit-content;
+    text-decoration: none;
+  }
+
+  @media( max-width : 800px){
+    .wrapper{
+      grid-template-rows: 5rem 1fr;
+      grid-template-columns: 1fr;
+      overflow: scroll;
+    }
+    .title-bar{
+      grid-row: 1;
+      width: 100vw;
+      height: 3rem;
+      margin-bottom: 1px solid white;
+      padding: 1rem 0 1rem 0;
+      border: none;
+    }
+    .content{
+      grid-row: 2;
+      grid-column: 1;
+      display: flex;
+      flex-direction: column;
+      height: auto;
+      justify-content: space-between;
+      padding-top: 2rem;
+    }
+    .nav-buttons{
+      grid-column: 1;
+      flex-direction: row;
+      justify-content: space-around;
+      width: 100vw;
+      margin-bottom: 1rem;
+      max-width: 100vw;
+    }
+    h1{
+      font-size: 1.5rem;
+      transform: rotate(0deg);
+      writing-mode: horizontal-tb;
+      margin: 1rem 1rem 0 1rem;
+      letter-spacing: 4px;
+    }
+    h2{
+      margin: 1rem 0;
+    }
+    article{
+      margin: 0 2rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      width: clamp(35ch, 90%, 65ch);
+    }
+    #increment, #decrement{
+      text-align: center;
+      padding: 0;
+      margin-bottom: 0.5rem;
+    }
+  }
+</style>
